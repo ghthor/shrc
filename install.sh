4e3b7f4c45a1af6e732eced761559ff6679c0536
Willa Drengwitz <ghthor@gmail.com>
2018-08-31 05:31:58 -0400

    Remove install scripts

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ..
-stow -t ~/ --ignore=.git/ --ignore=.gitmodules --ignore=.gitignore --ignore=install.sh vimrc/
--- uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd ..
-stow -D -t ~/ --ignore=.git/ --ignore=.gitmodules --ignore=.gitignore --ignore=install.sh vimrc/

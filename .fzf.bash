194bbe3b606ede0e9e84d45ab68d5d468f019eff
Willa Drengwitz <wdrengwitz@foursquare.com>
2018-08-31 11:44:56 -0400

    Setup OSX/Linux fzf env

--- .fzf.bash
+++ .fzf.bash
@@ -1,2 +1,19 @@
-source /usr/share/fzf/completion.bash
-source /usr/share/fzf/key-bindings.bash
+#!/bin/bash
+if [ "$(uname)" = "Darwin" ]; then
+  # Setup homebrew fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    export PATH="$PATH:/usr/local/opt/fzf/bin"
+  fi
+
+  # Auto-completion &&
+  # key bindings
+  # ------------
+  [[ $- == *i* ]] &&
+    source /usr/local/opt/fzf/shell/completion.bash 2>/dev/null &&
+    source /usr/local/opt/fzf/shell/key-bindings.bash
+
+else
+  source /usr/share/fzf/completion.bash
+  source /usr/share/fzf/key-bindings.bash
+fi

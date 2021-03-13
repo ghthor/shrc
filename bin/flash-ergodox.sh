7219db1c63ba01e9201ffb604f8f6958f9dc9957
Will Drengwitz <ghthor@gmail.com>
2021-03-13 12:19:19 -0500

    Fix exe name in ergodox flashing script

--- bin/flash-ergodox.sh
+++ bin/flash-ergodox.sh
@@ -5,4 +5,4 @@ set -xeuo pipefail
 QMK_ROOT=${QMK_ROOT:-"$HOME/src/hid/qmk_firmware"}
 
 (cd $QMK_ROOT && make ergodox_ez:khutulun)
-teensy_loader_cli -v -mmcu=atmega32u4 -w ${QMK_ROOT}/ergodox_ez_khutulun.hex
+teensy-loader-cli -v -mmcu=atmega32u4 -w ${QMK_ROOT}/ergodox_ez_khutulun.hex

c07e47105aa9f7e25d08c2ec83af34e571c17a77
Will Drengwitz <ghthor@gmail.com>
2024-02-02 06:32:08 -0500

    [nix] add tailscale

--- nix/cryptnix/configuration.nix
+++ nix/cryptnix/configuration.nix
@@ -58,6 +58,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  services.tailscale.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ghthor = {
     isNormalUser = true;

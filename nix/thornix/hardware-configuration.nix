538218834392d9b7dd65e472cd64f9c9597ee7d8
Will Drengwitz <ghthor@gmail.com>
2024-02-07 09:13:06 -0500

    [nix] setup laptop power daemon

--- nix/thornix/hardware-configuration.nix
+++ nix/thornix/hardware-configuration.nix
@@ -46,4 +46,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.system76.kernel-modules.enable = true;
+  hardware.system76.enableAll = true;
 }

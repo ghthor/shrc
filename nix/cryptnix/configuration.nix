b3662ca93944885e7af8ae82b1bc32698358b680
Will Drengwitz <ghthor@gmail.com>
2024-02-12 13:24:04 -0500

    [nix] fix bitmap fonts

--- nix/cryptnix/configuration.nix
+++ nix/cryptnix/configuration.nix
@@ -186,6 +186,7 @@
       (nerdfonts.override { fonts = [ "Hack" ]; })
     ];
     fontconfig = {
+      useEmbeddedBitmaps = true;
       localConf = ''
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
